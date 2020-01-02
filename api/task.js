8c8197253f8eacb12fae7d12f21e2204da4a466b
Luis Cipriani <cunhaluisg5@gmail.com>
2020-01-02 17:45:58 -0300

    Criada API de Tarefas (Tasks)

--- api/task.js
+++ api/task.js
@@ -28,7 +28,7 @@ module.exports = app => {
 
     const remove = (req, res) => {
         app.db('tasks')
-            .where({ id: req.params.id, userid: req.user.id })
+            .where({ id: req.params.id, userId: req.user.id })
             .del()
             .then(rowsDeleted => {
                 if (rowsDeleted > 0) {
@@ -40,4 +40,30 @@ module.exports = app => {
             })
             .catch(err => res.status(400).json(err))
     }
+
+    const updateTaskDoneAt = (req, res, doneAt) => {
+        app.db('tasks')
+            .where({ id: req.params.id, userId: req.user.id })
+            .update({ doneAt })
+            .then(_ => res.status(204).send())
+            .catch(err => res.status(400).json(err))
+    }
+
+    const toggleTask = (req, res) => {
+        app.db('tasks')
+            .where({ id: req.params.id, userId: req.user.id })
+            .first()
+            .then(task => {
+                if (!task) {
+                    const msg = `Task com id ${req.params.id} não encontrada.`
+                    return res.status(400).send(msg)
+                }
+
+                const doneAt = task.doneAt ? null : new Date()
+                updateTaskDoneAt(req, res, doneAt)
+            })
+            .catch(err => res.status(400).json(err))
+    }
+
+    return { getTasks, save, remove, toggleTask }
 }
